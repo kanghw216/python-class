e437c5e891d32b558a7d13221a03c93ea4ab5ede
kanghw <rkdgeumchi22@gmail.com>
2026-05-13 09:54:09 +0900

    updateOne

--- 탐색/Binary_search.py
+++ 탐색/Binary_search.py
@@ -3,7 +3,7 @@ def binary_search(arr, target):
   high = len(arr) - 1
 
   while low <= high:
-    mid = (low + high)
+    mid = (low + high) // 2
     if arr[mid] == target:
       return mid
     elif arr[mid] > target:
